b1568bbf24228dc1fd719e697f4b29b616e4d9bb
Guillaume DOTT <guillaume.dott@lafourmi-immo.com>
2013-09-03 17:01:22 +0200

    Add to_hayes method to some basic classes

---
+++ lib/biju/to_hayes.rb
@@ -0,0 +1,19 @@
+class Object
+  def to_hayes; "\"#{self.to_s}\""; end
+end
+
+class Fixnum
+  def to_hayes; self.to_s; end
+end
+
+class TrueClass
+  def to_hayes; '1'; end
+end
+
+class FalseClass
+  def to_hayes; '0'; end
+end
+
+class Array
+  def to_hayes; self.map(&:to_hayes).join(','); end
+end
